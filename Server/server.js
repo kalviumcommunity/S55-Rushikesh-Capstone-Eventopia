cabad7b6e3c7dea44bfbf70d7c04d4217db81a4e
rushikeshzope <rushikesh.zope@gmail.com>
2024-03-26 12:33:22 +0530

    worked on Codium feedback

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.get('/',(req,res)=>{
     res.send('Server deployed')
 })
 
-app.listen(process.env.PORT || 3000 , ()=>{
+app.listen(PORT , ()=>{
  console.log('success')
 })
 
