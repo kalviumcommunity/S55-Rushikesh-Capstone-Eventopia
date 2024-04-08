15f113f49bf1284c11b9d3619e4ad84b5c8043c6
rushikeshzope <rushikesh.zope@gmail.com>
2024-04-08 13:52:02 +0530

    worked on use effect as told by codium AI

--- Client/eventopia/src/components/home.jsx
+++ Client/eventopia/src/components/home.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './navbar';
 import Seats from '../assets/seats.png';
-import Rightarrow from '../assets/rightarrow.png';
-import Cross from '../assets/cross.png';
+import Rightarrow from '../assets/rightarrow.png'
+import Cross from '../assets/cross.png'
 import './home.css';
 
 function Home() {
@@ -13,47 +13,51 @@ function Home() {
   };
 
   useEffect(() => {
+
     const toggleScrollbar = () => {
       document.body.style.overflow = showBookContainer ? 'hidden' : 'auto';
     };
+
     toggleScrollbar();
 
     return () => {
-      document.body.style.overflow = 'auto';
+      if (!showBookContainer) {
+        document.body.style.overflow = 'auto';
+      }
     };
   }, [showBookContainer]);
 
   return (
     <>
-      {showBookContainer && <div className='overlay'></div>}
-      <div className={`bookcontainer ${showBookContainer ? 'show' : ''}`}>
+      {showBookContainer && <div className='overlay'></div>} 
+      <div className='bookcontainer' style={{ display: showBookContainer ? 'block' : 'none' }}>
         <div className='myticketbox'>
           <p className='mytickettxt'>MY TICKET</p>
-          <img src={Cross} alt="" className='cross' onClick={() => toggleBookContainer(false)} />
+          <img src={Cross} alt="" className='cross' onClick={() => toggleBookContainer(false)}/>
           <span className='items'>1 item</span>
         </div>
         <div className='grid-container'>
-          <div className='container'>
-            <div className='eventheadline'>
-              <span>DSA BOOTCAMP 2.0</span>
+        <div className='container'>
+          <div className='eventheadline'>
+            <span>DSA BOOTCAMP 2.0</span>
+          </div>
+          <div className='eventdetails'>
+            <div className='eventdetailsbolden'>
+              <p>DATE: <span className='details'>12th-15th March</span> </p>
+              <p>LOCATION: <span className='details'>SOC 519</span> </p>
+              <p>TIMING: <span className='details'>10AM-12:30PM</span></p>
+              <p>ORGANIZERS: <span className='details'>GFG, CODECHEF</span></p>
             </div>
-            <div className='eventdetails'>
-              <div className='eventdetailsbolden'>
-                <p>DATE: <span className='details'>12th-15th March</span> </p>
-                <p>LOCATION: <span className='details'>SOC 519</span> </p>
-                <p>TIMING: <span className='details'>10AM-12:30PM</span></p>
-                <p>ORGANIZERS: <span className='details'>GFG, CODECHEF</span></p>
-              </div>
-              <div className='seatcontainer'><span><img src={Seats} alt="" className='seatsimg' /><span className='seats'>150</span></span><span className='pricetxt'>FREE</span></div>
-              <div className="containerbtn">
-                <button id='viewposterbtn'>VIEW POSTER</button>
-                <button id='bookbtn' onClick={() => toggleBookContainer(true)}> <span className='booktxt'>BOOK</span> <img src={Rightarrow} alt="" className='rightarrowimg' /></button>
-              </div>
+            <div className='seatcontainer'><span><img src={Seats} alt="" className='seatsimg' /><span className='seats'>150</span></span><span className='pricetxt'>FREE</span></div>
+            <div className="containerbtn">
+              <button id='viewposterbtn'>VIEW POSTER</button>
+              <button id='bookbtn' onClick={() => toggleBookContainer(true)}> <span className='booktxt'>BOOK</span> <img src={Rightarrow} alt="" className='rightarrowimg' /></button>
             </div>
           </div>
         </div>
-        <div className='bookcontainerbtncontainer'>
-          <button className='cancelticket' onClick={() => toggleBookContainer(false)}>CANCEL TICKET</button>
+      </div>
+        <div className='bookcontainerbtncontainer'>  
+          <button className='cancelticket' onClick={() => toggleBookContainer(false)} >CANCEL TICKET</button>
           <button className='confirmticket'>CONFIRM TICKET</button>
         </div>
       </div>
