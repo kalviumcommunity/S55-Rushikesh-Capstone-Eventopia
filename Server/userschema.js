32116d7e06bb3f3bc9048c8083fae50b316f06ef
rushikeshzope <rushikesh.zope@gmail.com>
2024-04-25 13:42:35 +0530

    createdschema

--- Server/userschema.js
+++ Server/userschema.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     username: String,
     password: String
-    
 });
 const userModel = mongoose.model("credentials", userSchema);
 module.exports = {userModel};
