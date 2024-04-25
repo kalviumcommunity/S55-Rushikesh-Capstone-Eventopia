e21dff850dd03c821b736e9fc31c31cb9183e529
rushikeshzope <rushikesh.zope@gmail.com>
2024-04-25 17:16:51 +0530

    implemented get request

--- Server/routes.js
+++ Server/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getDataFromDatabase } = require("./db.js");
 const {dataModel} = require("./schema.js");
+const {userModel} = require("./userschema.js")
 
 router.get("/data", async (req, res) => {
   try {
@@ -23,5 +24,39 @@ router.get('/list', async (req,res) => {
       console.log(err)
   }
 })
+
+router.get('/', (req, res) => {
+  res.send('Server Deployed Successfully');
+});
+
+router.post('/signup',async (req,res)=>{
+  try{
+      const {username,password} = req.body
+
+
+          const response = await userModel.create(user)
+          res.status(200).send(response);
+  }
+  catch(err){
+      console.log("Error in signing up user",err);
+  }
+})
+
+router.post('/signin',async (req,res)=>{
+  try{
+          const {username,password} = req.body
+          const response = await userModel.findOne({username,password})
+          if(!response){
+              res.status(500).send("invalid user credentials")
+          }
+          
+          res.status(200).send(response)
+  }
+  catch(err){
+      console.log("Login failed",err);
+  }
+})
+
+
 
 module.exports = router;
